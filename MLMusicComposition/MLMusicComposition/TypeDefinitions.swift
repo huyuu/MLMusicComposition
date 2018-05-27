1281a98305d4c71d4ff92d31df1d2f175c990bb1
江宇揚 <yuyang@kousakaiage-no-MacBook.local>
2018-05-27 14:56:00 +0900

    Add new lines to xcode. (5/27/2018)

--- MLMusicComposition/MLMusicComposition/TypeDefinitions.swift
+++ MLMusicComposition/MLMusicComposition/TypeDefinitions.swift
@@ -18,25 +18,35 @@ let notesDictionaryInFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A
 
 // MARK: - struct Note
 
-struct Note {
+struct Note: Equatable {
     let noteValue: NoteValue
     let duration: Duration
 }
 
 // MARK: - class Cord
 
-class Cord {
+class Cord: Equatable {
+    static func == (lhs: Cord, rhs: Cord) -> Bool {
+        if lhs.cordType == rhs.cordType,
+            lhs.key == rhs.key {
+            return true
+        } else {
+            return false
+        }
+    }
+    
     let key: NoteValue // the key of the cord (ex: C)
     let cordType: String // cord type (ex: maj7)
     let keyNotes: [NoteValue] // key notes of the cord (ex: C, E, G for Cmaj)
     let tensions: [NoteValue] // tensions of the specific key (ex: D, F#, B for Cmaj7)
+    let duration: Duration
     
     
     // auxiliary static func for init()
     
     static func appendNotesFor(_ key: String, distances: [Int]) -> [NoteValue] {
         let keyIndex: Int = notesDictionaryInSharp.index(of: key) ?? notesDictionaryInFlat.index(of: key)!
-        return distances.flatMap{
+        return distances.map{
             let index = keyIndex + $0
             return index <= 12 ? notesDictionaryInSharp[index] : notesDictionaryInSharp[index-12]
         }
@@ -45,7 +55,7 @@ class Cord {
     
     // main initializer
     
-    init(_ cordName: String) {
+    init(_ cordName: String, duration: Duration) {
         let cordNameComponents = cordName.components(separatedBy: "_")
         guard cordNameComponents.count == 2 else { fatalError("Failed to split cord name into certain cord.") }
         
@@ -84,21 +94,23 @@ class Cord {
             fatalError("Cord type error.")
         }
         self.cordType = cordType
+        self.duration = duration
     }
 }
 
 
 // MARK: - class Measure
 
-class Measure {
-    var cord: Cord
+class Phrase {
+    var cords: [Cord]
     static let beats = 8
+    let measures: Int
     var notes: [Note]
     
-    init(cord: Cord, notes: [Note]) {
-        guard notes.reduce(0, { $0 + $1.duration }) == Measure.beats else { fatalError("The total timeValue doesn't meat the general beats.") }
-        
-        self.cord = cord
+    init(cords: [Cord], notes: [Note], measures: Int = 1) {
+        guard notes.reduce(0, { $0 + $1.duration }) == Phrase.beats * measures else { fatalError("The total timeValue doesn't meet the general beats.") }
+        self.measures = measures
+        self.cords = cords
         self.notes = notes
     }
 }
@@ -119,14 +131,70 @@ enum CordRelationship {
 // MARK: - class Environment
 
 class Environment {
-    let phrase: [Measure]
-    let relationship: CordRelationship
+    let phrase: Phrase
+    let currentCords: [Cord]
+    let relationships: [CordRelationship]
     
-    init( phrase: [Measure], relationship: CordRelationship ) {
+    init( phrase: Phrase, relationships: [CordRelationship], currentCords: [Cord] ) {
+        guard relationships.count == currentCords.count else {fatalError("relationships.count dismatch currentCords.count or phrase.count")}
         self.phrase = phrase
-        self.relationship = relationship
+        self.relationships = relationships
+        self.currentCords = currentCords
     }
 }
+
+
+// MARK: - class Evaluation
+
+class Reaction: Hashable {
+    var hashValue: Int
+    static func == (lhs: Reaction, rhs: Reaction) -> Bool {
+        if lhs.newPhrase.notes == rhs.newPhrase.notes,
+            lhs.newPhrase.cords == rhs.newPhrase.cords {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    let newPhrase: Phrase
+    let environment: Environment
+    
+    init(to environment: Environment) {
+        var generatedNotes = [Note]()
+        var randomDuration = Int(arc4random_uniform(8) + 1)
+        var randomNoteValue = Int(arc4random_uniform(12))
+        var beatsLeft = Phrase.beats * environment.phrase.measures
+        
+        while true {
+            let note = Note(noteValue: notesDictionaryInSharp[randomNoteValue], duration: randomDuration)
+            generatedNotes.append(note)
+            
+            if beatsLeft == 0 { break }
+            else {
+                beatsLeft -= randomDuration
+                randomNoteValue = Int(arc4random_uniform(12))
+                randomDuration = Int(arc4random_uniform(8) + 1)
+            }
+        }
+        
+        let cords = environment.currentCords
+        let measures = environment.phrase.measures
+        self.newPhrase = Phrase(cords: cords, notes: generatedNotes, measures: measures)
+        self.environment = environment
+        
+        var hashString = ""
+        for note in generatedNotes {
+            hashString.append(note.noteValue)
+            for _ in 1..<note.duration {
+                hashString.append("_")
+            }
+        }
+        self.hashValue = Int( hashString.unicodeScalars.map({$0.value}).reduce(0, { $0*10000 + $1 }) )
+    }
+}
+
+
 
 
 
--- MLMusicComposition/MLMusicComposition/ViewController.swift
+++ MLMusicComposition/MLMusicComposition/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+var Q = [Reaction: Double]()
 
 class ViewController: UIViewController {
 
